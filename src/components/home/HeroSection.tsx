9cbf730d561ea91391b3c9de561db91903917a91
jihansoft <jihanuddinsoft@gmail.com>
2025-07-25 23:48:49 +0600

    update hero text

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -6,8 +6,7 @@ export default function HeroSection() {
     <section className="lg:py-20 md:py-10 py-10 sm:py-10 bg-primary relative">
       <Hero
         title="Revitalize. Renew. Conquer the new and confident you."
-        description="<strong class='text-white'>Welcome to Hydra Aesthetics</strong> – your destination for IV hydration, vitamin infusions, hormone therapy, PRP treatments, and more. <br /><strong class='text-white'>Ask about our package deals to save on your journey to optimal health and beauty!"
-        subtitle="What type of services do you want? </strong>"
+        description="Welcome to <strong class='text-white'> Hydra Aesthetics</strong>—your destination for expertly crafted IV hydration, vitamin infusions, hormone therapy, PRP treatments, and more. We provide personalized wellness and aesthetic services designed to help you look and feel your absolute best. <br /><strong class='text-white'>Ask about our exclusive package deals and take the next step on your journey to lasting health and beauty.</strong>"
       />
     </section>
   );
