ff92e6169b8af5b2ebc21f8e1999af5d7f17ca73
jihansoft <jihanuddinsoft@gmail.com>
2025-07-27 01:48:06 +0600

    update hero gap change

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function HeroSection() {
   return (
-    <section className="lg:py-10 md:py-10 py-20 sm:py-10  bg-primary relative">
+    <section className="lg:py-0 md:py-10 py-20 sm:py-10  bg-primary relative">
       <Hero
         title="Revitalize. Renew. Conquer the new and confident you."
         description="Welcome to <strong class='text-white'> Hydra Aesthetics</strong>—your destination for expertly crafted IV hydration, vitamin infusions, hormone therapy, PRP treatments, and more. We provide personalized wellness and aesthetic services designed to help you look and feel your absolute best. <br /><strong class='text-white'>Ask about our exclusive package deals and take the next step on your journey to lasting health and beauty.</strong>"
