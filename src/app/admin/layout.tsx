9b16f1ecfe63146ecb8e0cf5d470dee9c8695c73
jihansoft <jihanuddinsoft@gmail.com>
2025-08-13 19:52:25 +0600

    Testing version

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,10 @@
 import { ReactNode } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { requireRole } from "@/lib/roleCheck";
 import { redirect } from "next/navigation";
+import { TbShoppingBagPlus } from "react-icons/tb"; // For the notification icon
+import Button from "@/common/Button";
 
 export default async function AdminLayout({
   children,
@@ -15,16 +18,58 @@ export default async function AdminLayout({
   }
 
   return (
-    <div className="flex min-h-screen">
-      <aside className="w-64 bg-gray-900 text-white p-4">
-        <h1 className="text-xl font-bold mb-6">Admin Panel</h1>
-        <nav className="space-y-3 flex flex-col">
-          <Link href="/admin">Dashboard</Link>
-          <Link href="/admin/products">Products</Link>
-          <Link href="/admin/users">Users</Link>
-        </nav>
-      </aside>
-      <main className="flex-1 p-6 bg-gray-50">{children}</main>
+    <div className="flex flex-col bg-[#252525]">
+      {/* Topbar */}
+      <div className="bg-[#2E2E2E] w-full">
+        <div className="container mx-auto flex justify-between items-center px-6 md:px-16 py-8">
+          <div className="text-2xl font-bold text-white">
+            <Link href="/" className="hover:text-[#d6b36b]">
+              <div className="flex items-center gap-x-4 justify-start">
+                <Image
+                  src="/images/logo.svg"
+                  alt="Hydra Aesthetics"
+                  width={150}
+                  height={150}
+                  className="w-24 h-24"
+                />
+                <h2 className="text-2xl hidden md:flex font-bold bg-gradient-to-r from-[#C4AC7B] via-[#EEE1BA] to-[#836539] bg-clip-text text-transparent">
+                  Hydra Aesthetics
+                </h2>
+              </div>
+            </Link>
+          </div>
+
+          <div className="hidden md:flex items-center gap-8">
+            <h3 className="text-[32px] font-inter font-bold text-white">
+              Admin Panel
+            </h3>
+          </div>
+
+          <div className=" flex justify-center items-center space-x-5">
+            <Link href="/order" className="col-span-1 flex gap-x-3 relative">
+              <span className="text-gradient text-[16px] font-bold font-inter cursor-pointer">
+                Check Orders
+              </span>
+              <TbShoppingBagPlus className="w-6 h-6 text-[#d6b36b] hover:text-[#cbb688] cursor-pointer" />
+              {/* {itemQuantity > 0 && (
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                {itemQuantity}
+              </span>
+            )} */}
+            </Link>
+            <div className="hidden md:flex">
+              <a
+                target="_blank"
+                href="https://www.myaestheticspro.com/BN/index.cfm?A78B66E7FFE2188433572F72D74E0F5F">
+                <Button text="Add Moderators" borderLeanr="gradient-border" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <main className="flex-1 p-6">{children}</main>
     </div>
   );
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,97 @@
-import React from 'react'
+"use client";
+import Button from "@/common/Button";
+import React from "react";
 
-export default function page() {
+export default function Page() {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className="min-h-screen text-white">
+      <div className="container mx-auto px-4 sm:px-6 md:px-16 py-8">
+        {/* Form grid */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          {/* Left side - Form */}
+          <div className="space-y-5">
+            {/* Title */}
+            <h1 className="text-3xl sm:text-[40px] font-extrabold mb-8">
+              Provide Information
+            </h1>
+            <div>
+              <label className="block text-lg sm:text-[20px] font-bold mb-2 text-white">
+                Category Of Product
+              </label>
+              <select className="w-full bg-[#2E2E2E] border border-[#2E2E2E] rounded-xl px-4 py-3 sm:py-4 text-white focus:outline-none focus:border-[#C5AE7D]">
+                <option value="option1">IV Drips</option>
+                <option value="option2">Vitamins</option>
+              </select>
+            </div>
+            <div>
+              <label className="block text-lg sm:text-[20px] font-bold mb-2 text-white">
+                Name Of The Product
+              </label>
+              <input
+                type="text"
+                className="w-full bg-[#2E2E2E] border border-[#2E2E2E] rounded-xl px-4 py-3 text-white focus:outline-none focus:border-[#C5AE7D]"
+              />
+            </div>
+
+            <div>
+              <label className="block text-lg sm:text-[20px] font-bold mb-2 text-white">
+                Enter Price
+              </label>
+              <input
+                type="text"
+                className="w-full bg-[#2E2E2E] border border-[#2E2E2E] rounded-xl px-4 py-3 text-white focus:outline-none focus:border-[#C5AE7D]"
+              />
+            </div>
+
+            <div>
+              <label className="block text-lg sm:text-[20px] font-bold mb-2 text-white">
+                Description
+              </label>
+              <textarea
+                rows={5}
+                className="w-full bg-[#2E2E2E] border border-[#2E2E2E] rounded-xl px-4 py-3 text-white focus:outline-none focus:border-[#C5AE7D]"></textarea>
+            </div>
+          </div>
+
+          {/* Right side - Image Upload */}
+          <div className="bg-[#2E2E2E] rounded-lg p-6 sm:p-10 flex flex-col items-center justify-center mt-6 lg:mt-0">
+            <h3 className="text-2xl sm:text-[32px] font-inter font-bold text-white mb-6 sm:mb-10 text-center">
+              Upload Image
+            </h3>
+            <label className="w-full h-56 sm:h-80 border-2 border-dashed border-[#D6A553] flex flex-col items-center justify-center cursor-pointer bg-[#494D47] rounded-md hover:bg-gray-700 transition">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 text-[#D6A553] mb-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                />
+              </svg>
+              <span className="text-[#D6A553] text-center px-2">
+                Drag & drop or click to{" "}
+                <span className="underline">choose from files</span>
+              </span>
+              <input type="file" className="hidden" />
+            </label>
+          </div>
+        </div>
+
+        {/* Button */}
+        <div className="flex justify-center mt-8">
+          <a target="_blank" href="">
+            <Button
+              text="Add Product"
+              bg="bg-[#2E2E2E]"
+              borderLeanr="gradient-border"
+            />
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
