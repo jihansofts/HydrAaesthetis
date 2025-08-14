eb2de28e76dec1c21af3e234f406cf4d7c1b9fe9
jihansoft <jihanuddinsoft@gmail.com>
2025-08-15 00:03:44 +0600

    update url

--- src/app/(CommonLayout)/login/page.tsx
+++ src/app/(CommonLayout)/login/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const res = await fetch(`${baseUrl}/api/user/auth}`, {
+      const res = await fetch(`${baseUrl}/api/user/auth`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
