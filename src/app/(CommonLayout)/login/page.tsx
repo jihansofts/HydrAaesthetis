bf1a88d142b0edede8480d4b26361712f7309caa
jihansoft <jihanuddinsoft@gmail.com>
2025-08-17 02:11:52 +0600

    update login auth

--- src/app/(CommonLayout)/login/page.tsx
+++ src/app/(CommonLayout)/login/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const res = await fetch(`/api/user/auth`, {
+      const res = await fetch(`/user/auth`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
