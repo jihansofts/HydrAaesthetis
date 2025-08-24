3a010f74b096182082692f1238c91baf9dbf7cfd
skrifatuiux <skrifatuiux@gmail.com>
2025-08-25 01:01:14 +0600

    Description must be less than 130 characters

--- src/app/(CommonLayout)/checkout/page.tsx
+++ src/app/(CommonLayout)/checkout/page.tsx
@@ -28,7 +28,7 @@ const CheckOut = () => {
 
           <PayPalScriptProvider
             options={{
-              clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID_LIVE!,
+              clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
             }}>
             <PayPalButtons
               style={{ layout: "vertical", color: "gold" }}
